e2ce21b97f6795bc1ffb0d6a1d7b1e92ee7d5c76
jerometecson-main <zxctest8@zxcstream.xyz>
2026-08-02 21:49:34 +0800

    part 5

--- lib/allowed-referers.ts
+++ lib/allowed-referers.ts
@@ -7,6 +7,7 @@ const ALLOWED_REFERERS = [
   //BACKUP
   "https://backup-zxcstream-xyz.up.railway.app/",
   "https://backup-zxcprime-xyz.up.railway.app/",
+  "https://online-thin-prox-production.up.railway.app/",
 ];
 
 export const ALLOWED_ORIGINS = [
@@ -18,6 +19,7 @@ export const ALLOWED_ORIGINS = [
   //BACKUP
   "https://backup-zxcstream-xyz.up.railway.app",
   "https://backup-zxcprime-xyz.up.railway.app",
+  "https://online-thin-prox-production.up.railway.app",
 ];
 export function isValidReferer(referer: string): boolean {
   return ALLOWED_REFERERS.some((allowed) => referer.includes(allowed));
